385682c96f36154d15126fcd02c7c69f946ad413
isaac <isaa0396@stud.kea.dk>
2021-09-17 15:16:48 +0200

    Initial commit

---
+++ src/main/java/com/example/bigbowlxp/BigbowlxpApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigbowlxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigbowlxpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigbowlxpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bigbowlxp/BigbowlxpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigbowlxp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigbowlxpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
