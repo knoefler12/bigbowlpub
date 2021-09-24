d22cfc2b03ee5ee2fff4dea88f36d11c5fe4f07a
zagrell <sverrireinert@gmail.com>
2021-09-24 14:37:55 +0200

    bowling lane user story 1 test

---
+++ src/test/java/com/example/bigbowlxp/BowlingLaneApplicationTest.java
@@ -0,0 +1,29 @@
+package com.example.bigbowlxp;
+
+import com.example.bigbowlxp.models.BowlingLane;
+import com.example.bigbowlxp.services.BowlingService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class BowlingLaneApplicationTest {
+
+    @Autowired
+    BowlingService bowlingService;
+
+    @Test
+    void laneListTest(){
+        ArrayList<BowlingLane> lanes = bowlingService.getLanes();
+        assertThat(lanes.size() == 24);
+        assertThat(lanes.get(0).isChildLane());
+        assertThat(lanes.get(1).isChildLane());
+        assertThat(lanes.get(2).isChildLane());
+        assertThat(lanes.get(3).isChildLane());
+    }
+
+}
