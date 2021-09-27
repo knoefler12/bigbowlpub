4c61bff0c865f3118abd681a237d38ffa60ea20f
MadsKolbye <christiane1662@yahoo.dk>
2021-09-27 11:18:45 +0200

    Møller og Kolbye - Vi har oprettet: Controller, Service, Repository og Model til Equipment. Vi har derved også lavet en liste metode der viser listen over equipment, og vi har lavet backend til update. Vi mangler HTML side til vores backend update metode.

--- src/main/java/com/example/bigbowlxp/controllers/EquipmentController.java
+++ src/main/java/com/example/bigbowlxp/controllers/EquipmentController.java
@@ -5,10 +5,7 @@ import com.example.bigbowlxp.services.EquipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,7 +35,7 @@ public class EquipmentController {
         return "updateEquipment";
     }
 
-    @PatchMapping("equipment/{equipment_id}")
+    @PostMapping("equipment/{equipment_id}")
     public String updateEquipment(@ModelAttribute Equipment equipment){
         equipmentService.updateEquipment(equipment);
         return "equipment";
