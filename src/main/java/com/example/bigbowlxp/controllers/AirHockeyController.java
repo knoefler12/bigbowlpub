305071781a2d8341789e2c727c21f428b63ed73a
knoefler12 <55090565+knoefler12@users.noreply.github.com>
2021-10-18 14:33:44 +0200

    Update AirHockeyController.java

--- src/main/java/com/example/bigbowlxp/controllers/AirHockeyController.java
+++ src/main/java/com/example/bigbowlxp/controllers/AirHockeyController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+
+//test yoo
 @Controller
 public class AirHockeyController {
 
